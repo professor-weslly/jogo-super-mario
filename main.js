cf066b4fd8bc51faf7cb5371d02245eddcde7edc
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-08-26 10:33:46 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
 const jump = () => {
-  mario.classList.add('jump');
-}
-document.addEventListener('keydown', jump);
+  mario.classList.add("jump");
 
-const pipe
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }
+  ,500);
+}
+
+const loop = setInterval (() => {});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("keydown", jump);
